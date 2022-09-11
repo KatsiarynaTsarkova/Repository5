8a62ae4e4dede84439b77e42297011a607ca09f8
Kate <katya_tsarkov@mail.ru>
2022-09-11 19:52:06 +0300

    Test3

---
+++ Test3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа задает массив из 8 элементов и выводит их на экран
+int[] FillArrayNumber(int[] array)
+{
+    for( int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+return array;
+}
+
+void WriteArrayPrint(int[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    } 
+}
+int[] numbers = FillArrayNumber(new int[8]);
+WriteArrayPrint(numbers);
+
+
