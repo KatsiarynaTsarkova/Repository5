77c448a86fa97070757240588a97116351e1f40a
Kate <katya_tsarkov@mail.ru>
2022-09-11 19:15:24 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа принимает на вход два числа а и в, и возводит а в степень в.
+Console.WriteLine("Введите число A ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число размера степени В ");
+int b = int.Parse(Console.ReadLine());
+double c = Math.Pow(a, b);
+Console.WriteLine($"c ={c } ");
