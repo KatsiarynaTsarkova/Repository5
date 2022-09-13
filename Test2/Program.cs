9e125bf6c341a261bc12479091d952dd1b0e1524
Kate <katya_tsarkov@mail.ru>
2022-09-13 19:18:42 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+        // Программа принимает на вход число и выдает сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+while( a > 0)
+{
+    sum = sum + a % 10;
+    a = a / 10;
+} 
+Console.WriteLine("Сумма цифр: " + (sum));
+       
